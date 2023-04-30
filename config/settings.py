646080bd453947d19499566faef9d48c5f8c7abe
teacher-kiwi <kwi07@hanmail.net>
2023-04-30 22:14:00 +0900

    feat : settings.py에 db 정보 변경(sqlite3 -> postgresql)

--- config/settings.py
+++ config/settings.py
@@ -89,8 +89,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST"),
+        "PORT": "5432",
     }
 }
 
