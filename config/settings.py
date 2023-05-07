b4bee3fe5688bc1bf8cfb009c9d9eab440b57387
Junbo Koh <cjsn92@gmail.com>
2023-05-07 16:17:19 +0900

    feat: register local app at settings

--- config/settings.py
+++ config/settings.py
@@ -44,6 +44,8 @@ THIRD_PARTY_APPS = [
 
 LOCAL_APPS = [
     'teachercan.users',
+    'teachercan.students',
+    'teachercan.plans',
 ]
 
 INSTALLED_APPS = DJANGO_APPS + THIRD_PARTY_APPS + LOCAL_APPS
