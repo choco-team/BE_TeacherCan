d8b9cd791be4c3827dcac1375c1919769f4388d9
JSM000 <67637792+JSM000@users.noreply.github.com>
2023-08-05 15:43:54 +0900

    annotate-goormide-connecting-code

--- app/database.py
+++ app/database.py
@@ -3,15 +3,18 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from urllib.parse import quote
 
+# user = "can"
+# pwd = "1234" # 특수기호 전처리
+# host = "3.34.99.98"
+# port = "53866" # 구름 ide 에서 가져오기
+# db_url = f'mysql+pymysql://{user}:{pwd}@{host}:{quote(port)}/teachercan'
+# engine = create_engine(db_url)
+
 SQLALCHEMY_DATABASE_URL = "sqlite:///./db.sqlite3"
 
-user = "can"
-pwd = "1234" # 특수기호 전처리
-host = "3.34.99.98"
-port = "53866" # 구름 ide 에서 가져오기
-db_url = f'mysql+pymysql://{user}:{pwd}@{host}:{quote(port)}/teachercan'
-
-engine = create_engine(db_url)
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
