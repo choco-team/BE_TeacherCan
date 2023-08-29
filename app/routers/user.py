48b1dd95da59c05a9d9c540787527f13fe5ab16c
kiwi <kwi07@hanmail.net>
2023-08-29 11:36:17 +0900

    fix: 토큰 검증 과정 수정

--- app/routers/user.py
+++ app/routers/user.py
@@ -1,9 +1,9 @@
-from fastapi import Depends, HTTPException, APIRouter, Request, status
+from fastapi import Depends, APIRouter, status
 from sqlalchemy.orm import Session
 
 from .. import crud, schemas
 from ..dependencies import get_db
-from .auth import auth_scheme
+from .auth import verify_token
 
 router = APIRouter(prefix="/user", tags=["User"])
 
@@ -11,29 +11,18 @@ router = APIRouter(prefix="/user", tags=["User"])
 # 1.개인 정보 조회
 @router.get("/info", status_code=200, response_model=schemas.User)
 async def my_info(
-    request: Request, db: Session = Depends(get_db), _: str = Depends(auth_scheme)
+    db: Session = Depends(get_db), token_email: str = Depends(verify_token)
 ):
-    db_user = crud.get_user(db, email=request.state.email)
-    if not db_user:
-        raise HTTPException(
-            status_code=status.HTTP_404_NOT_FOUND, detail="Invalid Token"
-        )
+    db_user = crud.get_user(db, email=token_email)
     return db_user
 
 
 # 2.개인 정보 수정
 @router.put("/info", status_code=status.HTTP_201_CREATED, response_model=schemas.User)
 async def my_info(
-    request: Request,
     user: schemas.UserUpdate,
     db: Session = Depends(get_db),
-    _: str = Depends(auth_scheme),
+    token_email: str = Depends(verify_token),
 ):
-    db_user = crud.update_user(db, email=request.state.email, user=user)
+    db_user = crud.update_user(db, email=token_email, user=user)
     return db_user
-
-
-# TODO 명렬표
-@router.get("/student")
-async def test(db: Session = Depends(get_db)):
-    return {"test": "test_result"}
