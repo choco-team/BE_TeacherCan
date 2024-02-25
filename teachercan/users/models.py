a64a063fc989e68c11bb4768a3b7783a352d80e2
kiwi <kwi07@hanmail.net>
2024-02-25 18:11:18 +0900

    명렬표 모델 description 추가

--- teachercan/users/models.py
+++ teachercan/users/models.py
@@ -102,6 +102,7 @@ class StudentList(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     has_allergy = models.BooleanField(default=False)
+    description = models.CharField(null=True, max_length=200)
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
 
