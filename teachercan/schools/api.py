e1ebdac26e53e12d84df8af747237e94efb2c13c
kiwi <kwi07@hanmail.net>
2024-03-06 23:04:18 +0900

    school api 작업 중

--- teachercan/schools/api.py
+++ teachercan/schools/api.py
@@ -1,15 +1,15 @@
 from ninja import Router, Query
 
-from .schemas import SchoolIn
+from .schemas import SchoolIn, LunchIn
 
 router = Router(tags=["School"])
 
 
 @router.get("/list")
-def list_school(reqeust, payload=Query[SchoolIn]):
-    return ...
+def list_school(request, payload: Query[SchoolIn]):
+    return None
 
 
 @router.get("/lunch-menu")
-def list_lunch_menu(request, payload=Query[SchoolIn]):
+def list_lunch_menu(request, payload: Query[LunchIn]):
     return ...
--- teachercan/schools/schemas.py
+++ teachercan/schools/schemas.py
@@ -1,13 +1,20 @@
+from datetime import date
 from enum import Enum
 
 from ninja import Schema, Field
 
 
-# class DayOrWeek(str, Enum):
-#     day: "day"
+class DayOrWeek(str, Enum):
+    day = "day"
+    week = "week"
 
 
 class SchoolIn(Schema):
     schoolName: str = Field(..., alias="school_name")
     pageNumber: int = Field(1, alias="page_number")
     dataSize: int = Field(10, alias="data_size")
+
+
+class LunchIn(Schema):
+    type: DayOrWeek = Field(...)
+    date: date
