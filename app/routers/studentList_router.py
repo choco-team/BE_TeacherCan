e8b271908ca79d37df5b64c2453cc189ec9b8be6
JSM000 <67637792+JSM000@users.noreply.github.com>
2024-03-08 00:16:08 +0900

    conflict 수정

--- app/routers/studentList_router.py
+++ app/routers/studentList_router.py
@@ -162,26 +162,15 @@ def student_list(
     status_code=status.HTTP_200_OK,
     response_model=ResponseModel[str],
 )
-<<<<<<< HEAD:app/routers/studentList_router.py
 def put_student_list_main(
     update_main: studentList_schemas.UpdateMain,
     user: str = Depends(user)
 ):
-=======
-def put_student_list_main(update_main: schemas.UpdateMain, user: str = Depends(user)):
->>>>>>> dev:app/routers/student_list.py
     try:
         updated_student_list = StudentList.objects.get(id=update_main.id, user=user)
         tmp = StudentList.objects.get(user=user, is_main=True)
     except ObjectDoesNotExist:
-<<<<<<< HEAD:app/routers/studentList_router.py
-        print("Asdajdnakd")
         raise ex.NotExistStudentList()  
-    
-=======
-        raise ex.NotExistStudentList()
-
->>>>>>> dev:app/routers/student_list.py
     tmp.is_main = False
     updated_student_list.is_main = update_main.isMain
     tmp.save()
@@ -320,58 +309,4 @@ def student_list(
 #         ]
 #     }
 #     """
-<<<<<<< HEAD:app/routers/studentList_router.py
 #     return ResponseWrapper(crud.update_column(db, column, token_email))
-=======
-#     return ResponseWrapper(crud.update_column(db, column, token_email))
-
-
-# @router.get(
-#     "/{student_id}",
-#     status_code=status.HTTP_200_OK,
-#     response_model=ResponseModel[schemas.Student],
-# )
-# async def student(
-#     student_id: int,
-#     db: Session = Depends(get_db),
-#     token_email: str = Depends(user_email),
-# ):
-#     return ResponseWrapper(crud.get_student(db, token_email, student_id))
-
-
-# @router.post(
-#     "/",
-#     status_code=status.HTTP_201_CREATED,
-#     response_model=ResponseModel[schemas.Student],
-# )
-# async def student(
-#     list_id: int = Query(..., alias="listId"),
-#     student: schemas.StudentCreate = Body(...),
-#     db: Session = Depends(get_db),
-#     token_email: str = Depends(user_email),
-# ):
-#     return ResponseWrapper(crud.create_student(db, token_email, list_id, student))
-
-
-# @router.put(
-#     "/{student_id}",
-#     status_code=status.HTTP_200_OK,
-#     response_model=ResponseModel[schemas.Student],
-# )
-# async def student(
-#     student_id: int,
-#     student: schemas.StudentUpdate,
-#     db: Session = Depends(get_db),
-#     token_email: str = Depends(user_email),
-# ):
-#     return ResponseWrapper(crud.update_student(db, token_email, student_id, student))
-
-
-# @router.delete("/{student_id}", status_code=status.HTTP_204_NO_CONTENT)
-# async def student(
-#     student_id: int,
-#     db: Session = Depends(get_db),
-#     token_email: str = Depends(user_email),
-# ):
-#     return ResponseWrapper(crud.delete_student(db, token_email, student_id))
->>>>>>> dev:app/routers/student_list.py
